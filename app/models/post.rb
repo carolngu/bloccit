b0c0d6fbfcf013071aa6a9c94671e3bafffdb576
Carol <carolngu@gmail.com>
2016-03-07 20:21:17 -0800

    assignment 31 intro to CRUD: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  def self.spamify
+    posts = Post.all
+
+    posts.each_with_index do |post,index|
+      if index % 5 == 0
+        post.title = "SPAM"
+        post.save!
+      end
+    end
+  end
 end
