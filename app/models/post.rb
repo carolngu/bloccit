4034c3768e203722035e4e899c2bcdfabc87b7ff
Carol <carolngu@gmail.com>
2016-03-20 23:28:37 -0700

    Assignment 44 Favorites: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ActiveRecord::Base
   has_many :labels, through: :labelings
 
   default_scope { order('rank DESC') }
+  after_create :create_favorite
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -30,5 +31,12 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_favorite
+    user.favorites.create(post: self)
+    FavoriteMailer.new_post(self.user, self).deliver_now
   end
 end
